b003c1ec746f545a2d4af8328dddf0b46ca18289
Elvira Miasnykova <miasnykovaelvira@gmail.com>
2022-07-25 00:38:16 +0300

    first try task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const simpleLightBoxGallery = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+console.log(galleryItemsMarkup);
+simpleLightBoxGallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+// const galleryItemsContainer = document.querySelector('.gallery');
+// const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+// galleryItemsContainer.insertAdjacentHTML('beforeend',galleryItemsMarkup);
+
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({original, preview, description}) => {
+        return `<a class="gallery__item" href="${original}">
+        < img class="gallery__image" src = "${preview}"
+        alt = "${description}" />
+            </a>`}).join("");
+}
+
+function onClickLightBoxGallery(e) {
+    e.preventDefault();
+    if (e.nodeName) {
+        const imageOriginal = e.target.dataset.source;
+        const gallerySimpleBox = new SimpleLightbox('.gallery a', { captionDelay: '250', captionPosition: 'bottom', captionData: "alt" });
+    }
+}
+
+simpleLightBoxGallery.addEventListener('click', onClickLightBoxGallery);
+
+
 console.log(galleryItems);
