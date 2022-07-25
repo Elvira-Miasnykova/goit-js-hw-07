19c53214c2083161413e62ac06567992d3e17666
Elvira Miasnykova <miasnykovaelvira@gmail.com>
2022-07-26 00:23:02 +0300

    complete task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,31 +4,57 @@ import { galleryItems } from './gallery-items.js';
 const simpleLightBoxGallery = document.querySelector('.gallery');
 
 const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
-console.log(galleryItemsMarkup);
+//console.log(galleryItemsMarkup);
+//console.log(createGalleryItemsMarkup(galleryItems));
 simpleLightBoxGallery.insertAdjacentHTML('beforeend', galleryItemsMarkup);
 
-// const galleryItemsContainer = document.querySelector('.gallery');
-// const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
-// galleryItemsContainer.insertAdjacentHTML('beforeend',galleryItemsMarkup);
 
 
-function createGalleryItemsMarkup(galleryItems) {
-    return galleryItems.map(({original, preview, description}) => {
+
+function createGalleryItemsMarkup(images) {
+    return images.map(({preview, original, description}) => {
         return `<a class="gallery__item" href="${original}">
-        < img class="gallery__image" src = "${preview}"
-        alt = "${description}" />
-            </a>`}).join("");
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+        }).join("");
 }
+//console.log(createGalleryItemsMarkup);
+
+// function createGalleryItemsMarkup(images) {
+//     return images.map(({preview, original, description}) => {
+//         return `<div class="gallery__item">
+//         <a class="gallery__link" href="${original}">
+//             <img
+//                 class="gallery__image"
+//                 src="${preview}"
+//                 data-source="${original}"
+//                 alt="${description}"
+//             />
+//         </a>
+//         </div>`;
+//     }).join("");
+
+
+
 
 function onClickLightBoxGallery(e) {
+    //console.log(e.currentTarget);
+    //console.log(e.target);
     e.preventDefault();
-    if (e.nodeName) {
-        const imageOriginal = e.target.dataset.source;
-        const gallerySimpleBox = new SimpleLightbox('.gallery a', { captionDelay: '250', captionPosition: 'bottom', captionData: "alt" });
+    if (!e.target) {
+        return;
     }
-}
+    
+        //const imageOriginal = e.target.dataset.source;
+    const gallerySimpleBox = new SimpleLightbox('.gallery a', {
+        captionDelay: 250,
+        captionPosition: 'bottom',
+        captionsData: 'alt',
+    });
+    }
+//}
 
 simpleLightBoxGallery.addEventListener('click', onClickLightBoxGallery);
 
 
-console.log(galleryItems);
+//console.log(galleryItems);
